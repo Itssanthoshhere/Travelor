f7c07fa0295a77efd41d80f425287d41ad960cb1
Santhosh VS <santhosh02vs@gmail.com>
2026-05-15 20:25:42 +0530

    fix: specify video source type in Banner component

--- src/components/Index/Banner/Banner.jsx
+++ src/components/Index/Banner/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
           loop
           className="w-full h-full object-cover"
         >
-          <source src={video} />
+          <source src={video} type="video/mp4" />
         </video>
 
         <div className="absolute inset-0 flex justify-center items-center">
