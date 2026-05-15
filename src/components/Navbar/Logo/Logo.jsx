d13c13b45e42caac20c414b267c4f925bfe2eb9b
Santhosh VS <santhosh02vs@gmail.com>
2026-05-16 04:21:25 +0530

    fix: add smooth scroll behavior to logo link

--- src/components/Navbar/Logo/Logo.jsx
+++ src/components/Navbar/Logo/Logo.jsx
@@ -5,6 +5,7 @@ const Logo = ({ className = "" }) => {
     <>
       <Link
         to="/"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         className={`logo cursor-pointer text-2xl md:text-4xl text-white font-medium font-kaushan! ${className}`}
       >
         Travelo<span className="text-prim">r.</span>
